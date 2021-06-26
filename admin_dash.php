959d6bc5427abe1c4b84c23fcb5f5863d03456b1
ShahjalalShohag <33003206+ShahjalalShohag@users.noreply.github.com>
2021-06-26 10:37:56 +0600

    Update admin_dash.php

--- admin_dash.php
+++ admin_dash.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Point of Selling System | Dashboard</title>
+    <title>Tally Note | Dashboard</title>
 
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/fontawesome.min.css">
